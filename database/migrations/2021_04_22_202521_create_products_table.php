2515710c786170131ff6b27985c987c972234473
Wakoya Kumsa <waqo1234567@gmail.com>
2021-08-21 22:16:15 +0300

    cool project

---
+++ database/migrations/2021_04_22_202521_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('details')->nullable();
+            $table->double('price');
+            $table->double('shipping_cost');
+            $table->text('description');
+            $table->integer('category_id');
+            $table->unsignedInteger('brand_id')->unsigned();
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
